417022eba29767149e938d1a563c6af26be03a35
Hevin Mun <hevinxx@gmail.com>
2023-05-18 14:18:15 +0900

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("multiplatform") version "1.8.21"
+}
+
+group = "io.hevinxx"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
---
+++ src/jvmMain/kotlin/io/hevinxx/rational/Rational.kt
@@ -0,0 +1,4 @@
+package io.hevinxx.rational
+
+class Rational {
+}
